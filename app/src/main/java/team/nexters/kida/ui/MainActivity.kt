5fb6e005394913cf1b2de4e1c076c479a5ab0781
ojh102 <ojh102@gmail.com>
2022-01-15 23:54:57 +0900

    navigation 설정

--- app/src/main/java/team/nexters/kida/ui/MainActivity.kt
+++ app/src/main/java/team/nexters/kida/ui/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
+import team.nexters.kida.ui.list.ListScreen
 import team.nexters.kida.ui.theme.Theme
+import team.nexters.kida.util.Routes
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -14,10 +21,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Theme {
-                Surface {
-                    Text(text = "hello")
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.LIST
+                ) {
+                    composable(Routes.LIST) {
+                        ListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.WRITE + "?diaryId={diaryId}",
+                        arguments = listOf(diaryIdArgument())
+                    ) {
+
+                    }
+
+                    composable(
+                        route = Routes.DETAIL + "?diaryId={diaryId}",
+                        arguments = listOf(diaryIdArgument())
+                    ) {
+
+                    }
                 }
             }
         }
+    }
+
+    private fun diaryIdArgument() = navArgument(name = "diaryId") {
+        type = NavType.IntType,
+        defaultValue = -1
     }
 }
