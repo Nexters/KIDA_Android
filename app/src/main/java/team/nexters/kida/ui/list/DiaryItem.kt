2bd7d7274d04450ff2e3e1a60a764ccb720fdd65
kutta97 <yhjin04170@gmail.com>
2022-01-24 05:32:11 +0900

    Diary Item UI 구현

--- app/src/main/java/team/nexters/kida/ui/list/DiaryItem.kt
+++ app/src/main/java/team/nexters/kida/ui/list/DiaryItem.kt
@@ -1,19 +1,18 @@
 package team.nexters.kida.ui.list
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import team.nexters.kida.data.diary.Diary
-import team.nexters.kida.ui.theme.Typography
+import team.nexters.kida.ui.theme.*
 
 @Composable
 fun DiaryItem(
@@ -21,32 +20,65 @@ fun DiaryItem(
     onEvent: (ListEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Row(
+    Surface(
         modifier = modifier
-            .clickable { onEvent(ListEvent.OnDiaryClick(diary)) }
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
+            .clickable { onEvent(ListEvent.OnDiaryClick(diary)) },
+        shape = RoundedCornerShape(10.dp),
+        color = White,
+        elevation = 4.dp,
     ) {
-        Column(
-            modifier = Modifier.weight(1F),
-            verticalArrangement = Arrangement.Center,
+        Row(
+            modifier = modifier.padding(all = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+            Column(
+                modifier = modifier
+                    .weight(1F)
+                    .padding(top = 6.dp),
+                verticalArrangement = Arrangement.Center,
             ) {
-                Text(
-                    text = diary.title,
-                    style = Typography.h4
-                )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = diary.keyword,
-                    style = Typography.body1
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = diary.title,
+                        style = TextStyle(
+                            color = Black,
+                            fontSize = 18.sp
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(5.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "#${diary.keyword}",
+                        style = TextStyle(
+                            color = Primary,
+                            fontSize = 12.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = diary.keyword,
+                        style = TextStyle(
+                            color = Disable,
+                            fontSize = 12.sp
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(17.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = diary.content,
+                        style = TextStyle(
+                            color = DarkGrey
+                        )
+                    )
+                }
             }
         }
     }
