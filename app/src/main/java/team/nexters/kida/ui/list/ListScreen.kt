8ce420b4681780bcc086ac3ff9939570a7b4ebb7
kutta97 <yhjin04170@gmail.com>
2022-01-24 07:48:46 +0900

    ListScreen TopAppBar UI 구현

--- app/src/main/java/team/nexters/kida/ui/list/ListScreen.kt
+++ app/src/main/java/team/nexters/kida/ui/list/ListScreen.kt
@@ -3,25 +3,21 @@ package team.nexters.kida.ui.list
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.insets.LocalWindowInsets
-import com.google.accompanist.insets.navigationBarsPadding
 import com.google.accompanist.insets.rememberInsetsPaddingValues
 import com.google.accompanist.insets.ui.TopAppBar
 import kotlinx.coroutines.flow.collect
@@ -46,30 +42,37 @@ fun ListScreen(
     }
     Scaffold(
         scaffoldState = scaffoldState,
+        backgroundColor = BackGround,
         topBar = {
             TopAppBar(
-                title = { Text(text = "내 일기") },
+                title = {
+                    Box(Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "내 일기",
+                            style = TextStyle(
+                                color = Black,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center
+                            ),
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                },
                 backgroundColor = MaterialTheme.colors.background,
                 contentPadding = rememberInsetsPaddingValues(
                     insets = LocalWindowInsets.current.statusBars,
                     applyBottom = false,
-                )
+                ),
+                actions = {
+                    IconButton(onClick = {
+                        viewModel.onEvent(ListEvent.OnWriteClick)
+                    }) {
+                        Icon(Icons.Default.Add, null)
+                    }
+                }
             )
         },
-        floatingActionButton = {
-            FloatingActionButton(
-                modifier = Modifier.navigationBarsPadding(),
-                onClick = {
-                    viewModel.onEvent(ListEvent.OnWriteClick)
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    "Add"
-                )
-            }
-        },
-        backgroundColor = BackGround
     ) {
        Column(
             modifier = Modifier
