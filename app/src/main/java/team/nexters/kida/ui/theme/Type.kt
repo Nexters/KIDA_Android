8cb46cfb9ec002b63e216099262d83496247e770
seunghee63 <0603yang@naver.com>
2022-01-31 22:04:12 +0900

    feat: Fontfamily 추가

--- app/src/main/java/team/nexters/kida/ui/theme/Type.kt
+++ app/src/main/java/team/nexters/kida/ui/theme/Type.kt
@@ -2,9 +2,24 @@ package team.nexters.kida.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import team.nexters.kida.R
+
+val gmarketSansFamily = FontFamily(
+    Font(R.font.gmarket_sans_light, FontWeight.Light),
+    Font(R.font.gmarket_sans_medium, FontWeight.Normal),
+    Font(R.font.gmarket_sans_bold, FontWeight.Bold)
+)
+
+val notoSansFamily = FontFamily(
+    Font(R.font.noto_sans_thin, FontWeight.Thin),
+    Font(R.font.noto_sans_light, FontWeight.Light),
+    Font(R.font.noto_sans_regular, FontWeight.Normal),
+    Font(R.font.noto_sans_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
