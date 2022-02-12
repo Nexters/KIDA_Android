ab64fca3601b396cf464032b5db4f97fc517da8f
Jaehwan Oh (Bob) <ojh102@gmail.com>
2022-02-12 14:44:35 +0900

    키워드 하루에 한번만 뽑도록 수정

--- app/src/main/java/team/nexters/kida/ui/splash/SplashViewModel.kt
+++ app/src/main/java/team/nexters/kida/ui/splash/SplashViewModel.kt
@@ -5,22 +5,33 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import team.nexters.kida.data.diary.DiaryRepository
 import team.nexters.kida.ui.Screen
 import team.nexters.kida.util.UiEvent
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor() : ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val diaryRepository: DiaryRepository
+) : ViewModel() {
 
     private val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
 
     init {
         viewModelScope.launch {
-            delay(1000)
-            sendUiEvent(UiEvent.Navigate(Screen.Keyword.route))
+            diaryRepository.canWriteDiary()
+                .collectLatest { canWriteDiary ->
+                    delay(1000)
+                    if (canWriteDiary) {
+                        sendUiEvent(UiEvent.Navigate(Screen.Keyword.route))
+                    } else {
+                        sendUiEvent(UiEvent.Navigate(Screen.List.route))
+                    }
+                }
         }
     }
 
